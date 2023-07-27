808cbfb494c2c64eccddb839eeebea81ea1bd654
HKishere <939132879@qq.com>
2023-07-27 14:52:52 +0800

    首次提交

---
+++ FFmpegHelper/FFmpegHelper.cpp
@@ -0,0 +1,186 @@
+﻿// FFmpegHelper.cpp : 定义 DLL 的导出函数。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "FFmpegHelper.h"
+#include "iostream"
+
+
+// 这是导出变量的一个示例
+FFMPEGHELPER_API int nFFmpegHelper=0;
+
+// 这是导出函数的一个示例。
+FFMPEGHELPER_API int fnFFmpegHelper(void)
+{
+    return 0;
+}
+
+// 这是已导出类的构造函数。
+FFmpegHelper::FFmpegHelper()
+{
+	m_strURLorFileName.clear();
+	m_nVideoIndex = -1;
+	m_pBuffer;
+
+	m_avFormatCtx = NULL;
+	m_avCodec = NULL;
+	m_avCodec = NULL;
+	m_avSwsCtx = NULL;
+	m_dstFormat = AV_PIX_FMT_NONE;
+
+	m_avFrameDecodec = NULL;
+	m_avFrameRGB = NULL;
+	m_avPacket = NULL;
+
+	m_VideoH = -1;
+	m_VideoW = -1;
+
+    return;
+}
+
+void FFmpegHelper::SetURLOrFileName(char * pUrl)
+{
+	m_strURLorFileName.clear();
+	m_strURLorFileName = pUrl;
+#ifdef DEBUG
+	printf("URL = %s", m_strURLorFileName.c_str());
+#endif // DEBUG
+}
+
+int FFmpegHelper::InitFFmpeg()
+{
+	avformat_network_init();
+
+    // 打开码流前指定各种参数
+	AVDictionary *optionsDict = nullptr;
+	av_dict_set(&optionsDict, "buffer_size", "1024000", 0);
+    av_dict_set(&optionsDict, "rtsp_transport", "tcp", 0);
+	av_dict_set(&optionsDict, "stimeout", "2000000", 0);
+
+	m_avFormatCtx = avformat_alloc_context();
+	if (0 != avformat_open_input(&m_avFormatCtx, m_strURLorFileName.c_str(),NULL, &optionsDict))
+	{
+		std::cout << "Open input error! url:" << m_strURLorFileName << std::endl;
+		return 1;
+	}
+	if(avformat_find_stream_info(m_avFormatCtx, NULL) < 0)
+	{
+		std::cout << "Find stream info error!" << std::endl;
+		return 2;
+	}
+
+	for (unsigned int i = 0; i < m_avFormatCtx->nb_streams; i++)
+	{
+		if (m_avFormatCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+		{
+			m_nVideoIndex = i;
+			break;
+		}
+	}
+
+	if (-1 == m_nVideoIndex)
+	{
+		std::cout << "can't find a video stream.";
+		return 3;
+	}
+
+	//寻找一个匹配当前视频流的解码器
+	m_avCodecCtx = avcodec_alloc_context3(NULL);
+	avcodec_parameters_to_context(m_avCodecCtx, m_avFormatCtx->streams[m_nVideoIndex]->codecpar);
+	m_avCodec = avcodec_find_decoder(m_avCodecCtx->codec_id);
+	if (m_avCodec == NULL)
+	{
+		std::cout << "Codec not find.";
+		return 4;
+	}
+	//设置加速解码
+	m_avCodecCtx->lowres = m_avCodec->max_lowres;
+	m_avCodecCtx->flags2 |= AV_CODEC_FLAG2_FAST;
+
+	//打开解码器
+	if (avcodec_open2(m_avCodecCtx, m_avCodec, NULL) < 0)//为啥不设option
+	{
+		std::cout <<"Codec open failed.";
+		return 5;
+	}
+
+    m_VideoH = m_avFormatCtx->streams[m_nVideoIndex]->codecpar->height;
+    m_VideoW = m_avFormatCtx->streams[m_nVideoIndex]->codecpar->width;
+
+		// 6.预分配好内存
+	m_avPacket = av_packet_alloc();
+	m_avFrameDecodec = av_frame_alloc();
+	m_avFrameRGB = av_frame_alloc();
+    m_pBuffer = (uint8_t*)av_malloc(av_image_get_buffer_size(m_dstFormat, m_avCodecCtx->width, m_avCodecCtx->height, 1) * sizeof(uint8_t));
+	
+    // 7.缓存一帧数据
+	av_image_fill_arrays(m_avFrameDecodec->data, m_avFrameDecodec->linesize, m_pBuffer, m_dstFormat, m_avCodecCtx->width, m_avCodecCtx->height, 1);
+	return 0;
+}
+
+void FFmpegHelper::StartDecodec()
+{
+	std::thread tDecodec(&FFmpegHelper::DecdecThread, this);
+	tDecodec.join();
+}
+
+void FFmpegHelper::DecdecThread()
+{
+	m_dstFormat = AV_PIX_FMT_RGB24;
+	m_avSwsCtx = sws_getContext(m_avCodecCtx->width, m_avCodecCtx->height, m_avCodecCtx->pix_fmt,
+        m_VideoW, m_VideoH, m_dstFormat,
+        SWS_FAST_BILINEAR, NULL, NULL, NULL);
+	av_image_alloc(m_avFrameRGB->data, m_avFrameRGB->linesize, m_avCodecCtx->width, m_avCodecCtx->height, m_dstFormat, 1);
+
+	while (true) 
+	{
+		int nGotPicture = 0;
+        int nRet = av_read_frame(m_avFormatCtx, m_avPacket);
+        if (nRet >= 0)
+        {
+            if (m_avPacket->stream_index == m_nVideoIndex)
+            {
+                // 将AVPacket中的数据解码至pFrame
+                nRet = avcodec_send_packet(m_avCodecCtx, m_avPacket);
+                nGotPicture = avcodec_receive_frame(m_avCodecCtx, m_avFrameDecodec);
+                if (nRet < 0)
+                {
+                    std::cout << "Decode error.";
+                    continue;
+                }
+
+                if (nGotPicture < 0)
+                {
+                    std::cout << "Not get image.";
+                    continue;
+                }
+
+                // nGotPicture为0表示成功
+                if (!nGotPicture)
+                {
+                    sws_scale(m_avSwsCtx, (const uint8_t* const*)m_avFrameDecodec->data, m_avFrameDecodec->linesize, 0,
+                        m_avCodecCtx->height, m_avFrameRGB->data, m_avFrameRGB->linesize);
+
+                    // 调用回调
+					//CallBackInterface.FFmpegGetDecodecFrame((char*)m_avFrameDecodec->data[0], m_avCodecCtx->height, m_avCodecCtx->width);
+					CallBackInterface.FFmpegGetRGBFrame((char*)m_avFrameRGB->data[0], m_avCodecCtx->height, m_avCodecCtx->width);
+                    Sleep(1);
+                }
+            }
+        }
+        av_packet_unref(m_avPacket);
+        av_freep(m_avPacket);
+        Sleep(1);
+	}
+}
+
+void FFmpegInterface::FFmpegGetDecodecFrame(char * pData, int nHeight, int nWidth)
+{
+	char* p = pData;
+}
+
+void FFmpegInterface::FFmpegGetRGBFrame(char * pData, int nHeight, int nWidth)
+{
+	return;
+}
---
+++ FFmpegHelper/FFmpegHelper.h
@@ -0,0 +1,73 @@
+﻿// 下列 ifdef 块是创建使从 DLL 导出更简单的
+// 宏的标准方法。此 DLL 中的所有文件都是用命令行上定义的 FFMPEGHELPER_EXPORTS
+// 符号编译的。在使用此 DLL 的
+// 任何项目上不应定义此符号。这样，源文件中包含此文件的任何其他项目都会将
+// FFMPEGHELPER_API 函数视为是从 DLL 导入的，而此 DLL 则将用此宏定义的
+// 符号视为是被导出的。
+#ifdef FFMPEGHELPER_EXPORTS
+#define FFMPEGHELPER_API __declspec(dllexport)
+#else
+#define FFMPEGHELPER_API __declspec(dllimport)
+#endif
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libswscale/swscale.h>
+#include <libavdevice/avdevice.h>
+#include <libavutil/imgutils.h>
+}
+
+#include <string>
+#include <thread>
+
+#pragma comment(lib, "avcodec.lib")
+#pragma comment(lib, "avformat.lib")
+#pragma comment(lib, "avutil.lib")
+#pragma comment(lib, "swscale.lib")
+#pragma comment(lib, "avdevice.lib")
+
+struct FFMPEGHELPER_API FFmpegInterface {
+	void FFmpegGetDecodecFrame(char* pData, int nHeight, int nWidth);
+	void FFmpegGetRGBFrame(char* pData, int nHeight, int nWidth);
+};
+
+// 此类是从 dll 导出的
+class FFMPEGHELPER_API FFmpegHelper {
+public:
+	FFmpegHelper(void);
+	
+	void SetURLOrFileName(char* pUrl);
+	int InitFFmpeg();
+
+	void StartDecodec();
+
+	void DecdecThread();
+
+
+private:
+	std::string			m_strURLorFileName;	//视频URL或文件名
+	int					m_nVideoIndex;		//视频所在流索引
+	uint8_t *			m_pBuffer;			//缓存储解码后的图像
+
+	AVFormatContext*	m_avFormatCtx;		//封装、复用格式上下文
+	const AVCodec*		m_avCodec;			//解码器	
+	AVCodecContext*		m_avCodecCtx;		//解码器	上下文
+	SwsContext*			m_avSwsCtx;			//图像转换上下文
+	AVPixelFormat		m_dstFormat;		//转换目标像素格式
+
+	AVFrame*			m_avFrameDecodec;	//解码图像YUV
+	AVFrame*			m_avFrameRGB;		//转换后RGB图像
+	AVPacket*			m_avPacket;			//视频数据包
+
+	int					m_VideoH;			//视频高
+	int					m_VideoW;			//视频宽
+
+	FFmpegInterface		CallBackInterface;	//回调接口
+};
+
+extern FFMPEGHELPER_API int nFFmpegHelper;
+
+FFMPEGHELPER_API int fnFFmpegHelper(void);
